965befd6fc372f83ea4889340daa9f540f908598
bharath <bvirat13>
2019-08-19 01:57:19 +0530

    second commit

--- FlowerRecognition/FlowerRecognition/ViewController.swift
+++ FlowerRecognition/FlowerRecognition/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePickerInstance = UIImagePickerController();
+    @IBAction func CameraButtonTapped(_ sender: Any) {
+        
+        imagePickerInstance.sourceType = .photoLibrary;
+        imagePickerInstance.allowsEditing = false;
+        present(imagePickerInstance,animated:true,completion:nil);
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      if  let imageData = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+      {
+        imageView.image = imageData;
+        }
+        imagePickerInstance.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePickerInstance.delegate = self;
+        
     }
 
 

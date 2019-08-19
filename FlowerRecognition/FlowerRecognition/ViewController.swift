5494adb2562623613045c0bc2adf7fb47f7b9826
bharath <bvirat13>
2019-08-19 09:49:51 +0530

    Third commit upto image classifying and displaying on navigation finished

--- FlowerRecognition/FlowerRecognition/ViewController.swift
+++ FlowerRecognition/FlowerRecognition/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Vision
+import CoreML
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
@@ -14,7 +16,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     let imagePickerInstance = UIImagePickerController();
     @IBAction func CameraButtonTapped(_ sender: Any) {
         
-        imagePickerInstance.sourceType = .photoLibrary;
+        imagePickerInstance.sourceType = .camera;
         imagePickerInstance.allowsEditing = false;
         present(imagePickerInstance,animated:true,completion:nil);
         
@@ -22,11 +24,45 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
       if  let imageData = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
       {
-        imageView.image = imageData;
+        guard let ciImageData = CIImage(image: imageData)
+            else{
+                fatalError("cannot convert image to ci image");
         }
+        
+        imageView.image = imageData;
+        processImage(ciimageParam: ciImageData);
+        }
+    
         imagePickerInstance.dismiss(animated: true, completion: nil)
     }
+    func processImage(ciimageParam : CIImage)
+    {
     
+       guard let model = try? VNCoreMLModel(for: FlowerClassifier().model)
+        else{
+            fatalError("failed to import model");
+        }
+        let request = VNCoreMLRequest(model: model) { (success, error) in
+          
+            
+            print ("gomma \(success.results?.first as! VNClassificationObservation )");
+            let t = success.results?.first as! VNClassificationObservation ;
+            self.navigationItem.title = t.identifier;//success.results?.first. as! String;
+            
+        }
+    
+    
+        let handler = VNImageRequestHandler(ciImage: ciimageParam)//, options: [VNImageOptio])
+    do
+      {
+        try handler.perform([request]);
+        }
+    catch{
+        fatalError("failed to send vision request");
+        }
+        
+      
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
